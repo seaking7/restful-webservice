189ebea276cb3cf19de4cfbbbcd82d6c915efa2d
Kim Taekyung <seaking7@gmail.com>
2021-04-09 20:17:06 +0900

    Initial commit

---
+++ src/main/java/com/study/restfulwebservice/helloworld/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package com.study.restfulwebservice.helloworld;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+
+    private String message;
+
+}
---
+++ src/main/java/com/study/restfulwebservice/user/User.java
@@ -0,0 +1,15 @@
+package com.study.restfulwebservice.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private Integer id;
+    private String name;
+    private Date JoinDate;
+}
